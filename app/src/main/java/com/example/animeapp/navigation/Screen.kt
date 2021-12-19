d590f740fc03c2db8d329e26ffc53b2becb6b725
Damian <damian.ogorek.1996@gmail.com>
2021-12-19 22:29:19 +0100

    Updated package destination;

--- app/src/main/java/com/example/animeapp/navigation/Screen.kt
+++ app/src/main/java/com/example/animeapp/navigation/Screen.kt
@@ -1,6 +1,6 @@
 package com.example.animeapp.navigation
 
-import com.example.animeapp.navigation.util.Constants.DETAILS_ARGUMENT_KEY
+import com.example.animeapp.util.Constants.DETAILS_ARGUMENT_KEY
 
 sealed class Screen(val route: String) {
     object Splash : Screen("splash_screen")
