cd57d43121d0c2e882ea67ad993586e5142b6d29
Damian <damian.ogorek.1996@gmail.com>
2021-12-20 21:22:03 +0100

    Added type converters;

--- app/src/main/java/com/example/animeapp/data/local/database/AnimeDatabase.kt
+++ app/src/main/java/com/example/animeapp/data/local/database/AnimeDatabase.kt
@@ -2,12 +2,15 @@ package com.example.animeapp.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.animeapp.data.local.converter.DatabaseConverter
 import com.example.animeapp.data.local.dao.HeroDao
 import com.example.animeapp.data.local.dao.HeroRemoteKeyDao
 import com.example.animeapp.domain.model.Hero
 import com.example.animeapp.domain.model.HeroRemoteKey
 
 @Database(entities = [Hero::class, HeroRemoteKey::class], version = 1)
+@TypeConverters(DatabaseConverter::class)
 abstract class AnimeDatabase : RoomDatabase() {
     abstract fun heroDao(): HeroDao
     abstract fun heroRemoteKeyDao(): HeroRemoteKeyDao
