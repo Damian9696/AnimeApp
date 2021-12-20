9a04840730bc06903e4132fa085ae469d12f1e28
Damian <damian.ogorek.1996@gmail.com>
2021-12-20 23:05:56 +0100

    Added experimental annotation;

--- app/src/main/java/com/example/animeapp/MainActivity.kt
+++ app/src/main/java/com/example/animeapp/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.animeapp.navigation.SetupNavGraph
 import com.example.animeapp.ui.theme.AnimeAppTheme
+import com.google.accompanist.pager.ExperimentalPagerApi
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -14,6 +15,7 @@ class MainActivity : ComponentActivity() {
 
     private lateinit var navController: NavHostController
 
+    @ExperimentalPagerApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
