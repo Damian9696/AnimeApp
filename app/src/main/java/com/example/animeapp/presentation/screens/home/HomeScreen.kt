ee847e6cec79fb86d1e25a5c1d06b25934821c9e
Damian <damian.ogorek.1996@gmail.com>
2021-12-28 18:27:44 +0100

    Implemented view model; Created value of all heroes paging;

--- app/src/main/java/com/example/animeapp/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/animeapp/presentation/screens/home/HomeScreen.kt
@@ -2,9 +2,16 @@ package com.example.animeapp.presentation.screens.home
 
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.paging.compose.collectAsLazyPagingItems
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(
+    homeViewModel: HomeViewModel = hiltViewModel()
+) {
+
+    val allheroes = homeViewModel.getAllHeroes.collectAsLazyPagingItems()
+
     Scaffold(topBar = {
         HomeTopBar()
     }) {
