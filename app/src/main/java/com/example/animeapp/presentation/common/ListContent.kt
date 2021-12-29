a7f1dd616af533eb956e0b0e8181723a9cd0fa04
Damian <damian.ogorek.1996@gmail.com>
2021-12-29 19:51:45 +0100

    Implemented lazy column for items; Added preview functions;

--- app/src/main/java/com/example/animeapp/presentation/common/ListContent.kt
+++ app/src/main/java/com/example/animeapp/presentation/common/ListContent.kt
@@ -1,8 +1,11 @@
 package com.example.animeapp.presentation.common
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ContentAlpha
 import androidx.compose.material.MaterialTheme
@@ -17,23 +20,44 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import androidx.paging.compose.LazyPagingItems
+import androidx.paging.compose.items
 import coil.annotation.ExperimentalCoilApi
 import coil.compose.rememberImagePainter
+import com.example.animeapp.BuildConfig
 import com.example.animeapp.R
 import com.example.animeapp.domain.model.Hero
 import com.example.animeapp.navigation.Screen
 import com.example.animeapp.presentation.components.RatingWidget
 import com.example.animeapp.ui.theme.*
-import com.example.animeapp.util.Constants.BASE_URL
+import com.example.animeapp.util.Constants.LOREM_IPSUM_LONG
+import com.example.animeapp.util.Constants.LOREM_IPSUM_SHORT
 
+@ExperimentalCoilApi
 @Composable
 fun ListContent(
     heroes: LazyPagingItems<Hero>,
     navHostController: NavHostController
 ) {
-
+    LazyColumn(
+        contentPadding = PaddingValues(all = SMALL_PADDING),
+        verticalArrangement = Arrangement.spacedBy(space = SMALL_PADDING)
+    ) {
+        items(items = heroes,
+            key = { hero ->
+                hero.id
+            }
+        ) { hero ->
+            hero?.let {
+                HeroItem(hero = it, navHostController = navHostController)
+            }
+        }
+    }
 }
 
 @ExperimentalCoilApi
@@ -43,7 +67,7 @@ fun HeroItem(
     navHostController: NavHostController
 ) {
 
-    val painter = rememberImagePainter(data = "$BASE_URL${hero.image}") {
+    val painter = rememberImagePainter(data = "${BuildConfig.BASE_URL}${hero.image}") {
         placeholder(R.drawable.ic_placeholder)
         error(R.drawable.ic_placeholder)
     }
@@ -56,7 +80,7 @@ fun HeroItem(
             },
         contentAlignment = Alignment.BottomStart
     ) {
-        Surface(shape = Shapes.large) {
+        Surface(shape = RoundedCornerShape(size = LARGE_PADDING)) {
             Image(
                 modifier = Modifier.fillMaxSize(),
                 painter = painter,
@@ -100,7 +124,7 @@ fun HeroItem(
                         rating = hero.rating
                     )
                     Text(
-                        text = "($hero.rating)",
+                        text = "(${hero.rating})",
                         color = Color.White.copy(alpha = ContentAlpha.medium),
                         textAlign = TextAlign.Center
                     )
@@ -108,4 +132,48 @@ fun HeroItem(
             }
         }
     }
+}
+
+class HeroPreviewParameterProvider : PreviewParameterProvider<Hero> {
+    override val values = sequenceOf(
+        Hero(
+            id = 1,
+            name = LOREM_IPSUM_SHORT,
+            image = "",
+            about = LOREM_IPSUM_SHORT,
+            rating = 3.7,
+            power = 0,
+            month = "",
+            day = "",
+            family = emptyList(),
+            abilities = emptyList(),
+            natureTypes = emptyList()
+        ),
+        Hero(
+            id = 1,
+            name = LOREM_IPSUM_LONG,
+            image = "",
+            about = LOREM_IPSUM_LONG,
+            rating = 5.0,
+            power = 0,
+            month = "",
+            day = "",
+            family = emptyList(),
+            abilities = emptyList(),
+            natureTypes = emptyList()
+        )
+    )
+
+}
+
+@ExperimentalCoilApi
+@Preview
+@Preview(uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun HeroItemPreview(
+    @PreviewParameter(HeroPreviewParameterProvider::class) hero: Hero
+) {
+    HeroItem(
+        hero = hero, navHostController = rememberNavController()
+    )
 }
