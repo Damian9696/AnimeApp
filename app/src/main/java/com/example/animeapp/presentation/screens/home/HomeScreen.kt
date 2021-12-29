b10ee5cc38e4f8c6675c0474c903c9a3b6436569
Damian <damian.ogorek.1996@gmail.com>
2021-12-29 13:56:05 +0100

    Implemented Rating widget;

--- app/src/main/java/com/example/animeapp/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/animeapp/presentation/screens/home/HomeScreen.kt
@@ -1,9 +1,13 @@
 package com.example.animeapp.presentation.screens.home
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.compose.collectAsLazyPagingItems
+import com.example.animeapp.presentation.components.RatingWidget
+import com.example.animeapp.ui.theme.LARGE_PADDING
 
 @Composable
 fun HomeScreen(
@@ -15,5 +19,9 @@ fun HomeScreen(
     Scaffold(topBar = {
         HomeTopBar()
     }) {
+        RatingWidget(
+            modifier = Modifier.padding(all = LARGE_PADDING),
+            rating = 0.3
+        )
     }
 }
