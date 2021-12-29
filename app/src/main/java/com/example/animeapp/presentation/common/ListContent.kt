ed4463a98963126b2baf89b3cc470ae8c84fb470
Damian <damian.ogorek.1996@gmail.com>
2021-12-29 14:37:32 +0100

    Created list content widget; Create hero item widget;

---
+++ app/src/main/java/com/example/animeapp/presentation/common/ListContent.kt
@@ -0,0 +1,111 @@
+package com.example.animeapp.presentation.common
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.navigation.NavHostController
+import androidx.paging.compose.LazyPagingItems
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.rememberImagePainter
+import com.example.animeapp.R
+import com.example.animeapp.domain.model.Hero
+import com.example.animeapp.navigation.Screen
+import com.example.animeapp.presentation.components.RatingWidget
+import com.example.animeapp.ui.theme.*
+import com.example.animeapp.util.Constants.BASE_URL
+
+@Composable
+fun ListContent(
+    heroes: LazyPagingItems<Hero>,
+    navHostController: NavHostController
+) {
+
+}
+
+@ExperimentalCoilApi
+@Composable
+fun HeroItem(
+    hero: Hero,
+    navHostController: NavHostController
+) {
+
+    val painter = rememberImagePainter(data = "$BASE_URL${hero.image}") {
+        placeholder(R.drawable.ic_placeholder)
+        error(R.drawable.ic_placeholder)
+    }
+
+    Box(
+        modifier = Modifier
+            .height(HERO_ITEM_HEIGHT)
+            .clickable {
+                navHostController.navigate(Screen.Details.passHeroId(heroId = hero.id))
+            },
+        contentAlignment = Alignment.BottomStart
+    ) {
+        Surface(shape = Shapes.large) {
+            Image(
+                modifier = Modifier.fillMaxSize(),
+                painter = painter,
+                contentDescription = stringResource(R.string.hero_image),
+                contentScale = ContentScale.Crop
+            )
+        }
+        Surface(
+            modifier = Modifier
+                .fillMaxHeight(0.4f)
+                .fillMaxWidth(),
+            color = Color.Black.copy(alpha = ContentAlpha.medium),
+            shape = RoundedCornerShape(bottomStart = LARGE_PADDING, bottomEnd = LARGE_PADDING)
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(all = MEDIUM_PADDING)
+            ) {
+                Text(
+                    text = hero.name,
+                    color = MaterialTheme.colors.topAppBarContentColor,
+                    fontSize = MaterialTheme.typography.h5.fontSize,
+                    fontWeight = FontWeight.Bold,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    text = hero.about,
+                    color = Color.White.copy(alpha = ContentAlpha.medium),
+                    fontSize = MaterialTheme.typography.subtitle1.fontSize,
+                    maxLines = 3,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Row(
+                    modifier = Modifier.padding(top = SMALL_PADDING),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    RatingWidget(
+                        modifier = Modifier.padding(end = SMALL_PADDING),
+                        rating = hero.rating
+                    )
+                    Text(
+                        text = "($hero.rating)",
+                        color = Color.White.copy(alpha = ContentAlpha.medium),
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+        }
+    }
+}
