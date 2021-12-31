be65718b23a7d0fc39c8e63b2d6b87362212f3c3
Damian <damian.ogorek.1996@gmail.com>
2021-12-31 19:56:19 +0100

    Updated search screen by adding arguments; Implemented search tap bar;

--- app/src/main/java/com/example/animeapp/presentation/screens/search/SearchScreen.kt
+++ app/src/main/java/com/example/animeapp/presentation/screens/search/SearchScreen.kt
@@ -2,11 +2,29 @@ package com.example.animeapp.presentation.screens.search
 
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
 
 @Composable
-fun SearchScreen() {
-    Scaffold(topBar = {
+fun SearchScreen(
+    navHostController: NavHostController,
+    searchViewModel: SearchViewModel = hiltViewModel()
+) {
 
+    val searchQuery by searchViewModel.searchQuery
+
+    Scaffold(topBar = {
+        SearchTopBar(
+            text = searchQuery,
+            onTextChange = {
+                searchViewModel.updateSearchQuery(it)
+            },
+            onSearchClicked = {},
+            onCloseClicked = {
+                navHostController.popBackStack()
+            }
+        )
     }) {
 
     }
