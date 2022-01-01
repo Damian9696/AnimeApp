a21fdbb57c4f4b48bda978bed7d435aab4f7a182
Damian <damian.ogorek.1996@gmail.com>
2022-01-01 22:20:23 +0100

    Added annotation; Created fields of Hero PagingData type; Created function to updated state field when collect data;

--- app/src/main/java/com/example/animeapp/presentation/screens/search/SearchViewModel.kt
+++ app/src/main/java/com/example/animeapp/presentation/screens/search/SearchViewModel.kt
@@ -2,8 +2,21 @@ package com.example.animeapp.presentation.screens.search
 
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.example.animeapp.domain.model.Hero
+import com.example.animeapp.domain.use_cases.UseCases
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class SearchViewModel : ViewModel() {
+@HiltViewModel
+class SearchViewModel @Inject constructor(
+    private val useCases: UseCases
+) : ViewModel() {
 
     private val _searchQuery = mutableStateOf("")
     val searchQuery = _searchQuery
@@ -12,4 +25,14 @@ class SearchViewModel : ViewModel() {
         _searchQuery.value = query
     }
 
+    private val _searchedHeroes = MutableStateFlow<PagingData<Hero>>(PagingData.empty())
+    val searchedHeroes = _searchedHeroes
+
+    fun searchHeroes(query: String) {
+        viewModelScope.launch {
+            useCases.searchHeroesUseCase(query = query).cachedIn(viewModelScope).collect {
+                _searchedHeroes.value = it
+            }
+        }
+    }
 }
