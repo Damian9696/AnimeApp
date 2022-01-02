8d114211ee48622cdf43c8ee83900d97f630c217
Damian <damian.ogorek.1996@gmail.com>
2022-01-02 21:56:01 +0100

    Updated screen by adding view model; Implemented value of selected hero;

--- app/src/main/java/com/example/animeapp/presentation/screens/details/DetailsScreen.kt
+++ app/src/main/java/com/example/animeapp/presentation/screens/details/DetailsScreen.kt
@@ -1,11 +1,16 @@
 package com.example.animeapp.presentation.screens.details
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 
 @Composable
 fun DetailsScreen(
-    navHostController: NavHostController
+    navHostController: NavHostController,
+    detailsScreenViewModel: DetailsScreenViewModel = hiltViewModel()
 ) {
+
+    val selectedHero by detailsScreenViewModel.selectedHero
 
 }
