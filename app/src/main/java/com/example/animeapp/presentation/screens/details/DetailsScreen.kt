2961178fdbf931215c59db0af737a823b68b57d0
Damian <damian.ogorek.1996@gmail.com>
2022-01-02 23:39:41 +0100

    Implemented details content screen;

--- app/src/main/java/com/example/animeapp/presentation/screens/details/DetailsScreen.kt
+++ app/src/main/java/com/example/animeapp/presentation/screens/details/DetailsScreen.kt
@@ -1,16 +1,23 @@
 package com.example.animeapp.presentation.screens.details
 
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 
+@ExperimentalMaterialApi
 @Composable
 fun DetailsScreen(
     navHostController: NavHostController,
     detailsScreenViewModel: DetailsScreenViewModel = hiltViewModel()
 ) {
 
-    val selectedHero by detailsScreenViewModel.selectedHero
+    val selectedHero by detailsScreenViewModel.selectedHero.collectAsState()
 
+    DetailsContent(
+        navHostController = navHostController,
+        selectedHero = selectedHero
+    )
 }
